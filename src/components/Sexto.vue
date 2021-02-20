ec4cb01a5aaef0bb87d4a7114fbb537308758d53
jcgsr <cabralguerra@gmail.com>
2021-02-20 07:25:32 -0300

    sexto display fixed

--- src/components/Sexto.vue
+++ src/components/Sexto.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="sexto">
+  <div class="w-full">
     <h1>6º Ano</h1>
     <!-- component -->
-    <div class="w-full md:flex">
-      <div class="flex flex-col m-auto bg-white p-auto">
+    <div class="">
+      <div class="p-4 sm:m-6 bg-gray-50">
         <h2
-          class="flex px-5 mx-5 text-2xl font-bold text-red-500 lg:px-20 md:px-10 lg:mx-40 md:mx-20"
+          class="text-2xl font-bold text-center text-red-500"
         >
           Verb To Be
         </h2>
         <h2
-          class="flex mx-5 text-xl text-red-500 lg:px-20 md:px-10 lg:mx-40 md:mx-20"
+          class="text-xl text-center text-red-500"
         >
           Verbo Ser/Estar
         </h2>
 
-        <div class="flex pb-10 overflow-x-scroll hide-scroll-bar">
-          <div class="flex ml-10 flex-nowrap lg:ml-40 md:ml-20 ">
-            <div class="inline-block px-3">
+        <div class="pb-10">
+          <div class="block m-auto sm:flex sm:justify-around">
+            <div>
               <div
-                class="max-w-xs p-2 overflow-hidden text-blue-600 bg-white rounded-lg shadow-md w-72 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                class="p-2 m-auto mb-2 text-blue-600 bg-white rounded-lg shadow-md w-72 hover:shadow-xl transition-shadow duration-300 ease-in-out"
               >
                 <table>
                   <thead>
@@ -42,9 +42,9 @@
               </div>
             </div>
 
-            <div class="inline-block px-3">
+            <div>
               <div
-                class="max-w-xs p-2 overflow-hidden text-blue-600 bg-white rounded-lg shadow-md w-72 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                class="p-2 m-auto mb-2 text-blue-600 bg-white rounded-lg shadow-md w-72 hover:shadow-xl transition-shadow duration-300 ease-in-out"
               >
                 <table>
                   <thead>
@@ -67,9 +67,9 @@
               </div>
             </div>
 
-            <div class="inline-block px-3">
+            <div>
               <div
-                class="max-w-xs p-2 overflow-hidden text-blue-600 bg-white rounded-lg shadow-md w-72 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                class="p-2 m-auto mb-2 text-blue-600 bg-white rounded-lg shadow-md w-72 hover:shadow-xl transition-shadow duration-300 ease-in-out"
               >
                 <table>
                   <thead>
@@ -179,17 +179,3 @@ td,
 th {
   padding: 0.2rem;
 }
-.hide-scroll-bar {
-  -ms-overflow-style: none;
-
-  scrollbar-width: none;
-}
-
-.hide-scroll-bar::-webkit-scrollbar {
-  display: none;
-}
-.outer {
-  display: flex;
-  justify-content: space-around;
-}
-</style>
