a330bb00bc6a96fd8d9c7c4dea3f629dc19df415
jcgsr <cabralguerra@gmail.com>
2021-02-20 04:57:06 -0300

    6o ano again - be

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex-row">
+    <div class="flex-col">
+      <h1 class="p-3 text-2xl font-bold text-center text-red-500 h1s">
+        English Easy
+      </h1>
+      <div class="w-full buttons">
+        <button class="btn">
+        <router-link to="/sexto">
+          6º Ano
+        </router-link>
+        </button>
+        <button class="btn">
+          7º Ano
+        </button>
+        <button class="btn">
+          8º Ano
+        </button>
+        <button class="btn">
+          9º Ano
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style>
+.buttons {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 1rem;
+}
+</style>
---
+++ src/components/Info.vue
@@ -0,0 +1,10 @@
+<template>
+   <div>
+      <h1 class="p-3 text-2xl font-bold text-center text-red-500">Info</h1>
+   </div>
+</template>
+<script>
+export default {
+  name: "Info"
+}
+</script>
